a1cccc1c2909e7880395cfe8786d27faa2e2d45c
William Widener <97996739+William-James-Widener-III@users.noreply.github.com>
2025-01-16 18:45:37 -0600

    Added new bag object and populated bag
    Initialized a new bag object named "identityBag" and a new array named "identityItems", updated for loop to also add identityItems elements to identityBag

--- usingArrayBag.cpp
+++ usingArrayBag.cpp
@@ -11,13 +11,16 @@ void bagTester(ArrayBag<int>& bag);
 int main()
 {
 	ArrayBag<int> bag;
-	int items[] = {1, 33, 6, 9, 2, 65, 4, 29, 5, 8, 39, 88, 7, 25, 51, 3, 99, 14, 11, 10};
+	ArrayBag<int> identityBag;
 	
+	int items[] = {1, 33, 6, 9, 2, 65, 4, 29, 5, 8, 39, 88, 7, 25, 51, 3, 99, 14, 11, 10};
+	int identityItems[] = {0,1,2,3,4,5,6,7,8,9};
 	
 	cout << "Adding positive integers to the bag: " << endl;
 	for (int i = 0; i < 20; i++)
 	{
 		bag.add(items[i]);
+		identityBag.add(identityItems[i]);
 	}  // end for
 	
 	
